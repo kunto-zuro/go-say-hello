6fa0c628f5adde64d0f77c08641bd2559c3ef306
kurniawan.adityanto@nexsoft.co.id <kurniawan.adityanto@nexsoft.co.id>
2022-06-29 12:30:59 +0700

    perubahan function

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/kunto-zuro/go-say-hello
+module github.com/kunto-zuro/go-say-hello/v2
 
 go 1.17
--- say_hello.go
+++ say_hello.go
@@ -1,5 +1,5 @@
 package go_say_hello
 
-func Say_hello() string {
-	return "Hello World"
+func Say_hello(name string) string {
+	return "Hello " + name
 }
